c9794a697e11896abd85ba1c22816aef1faf978c
TatianaSham <156084120+TatianaSham@users.noreply.github.com>
2024-02-21 00:20:01 +0300

    Update main.py
    Capuccino

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import sys
-
+import sqlite3
 
 import PyQt5
 from PyQt5 import uic
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
 from PyQt5.QtWidgets import QApplication, QMainWindow
-
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 # from Main import
-
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         PyQt5.uic.loadUi('main.ui', self)
         self.initUI()
-
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.pushButton.clicked.connect(self.update_result)
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pushButton_2.clicked.connect(self.save_results)
+        self.modified = {}
+        self.titles = None
     def initUI(self):
         self.db = QSqlDatabase.addDatabase('QSQLITE')
         self.db.setDatabaseName('coffee.sqlite')
@@ -26,6 +31,50 @@ class Example(QMainWindow):
 
         self.view.setModel(self.model)
         self.view.move(10, 10)
+
+    def update_result(self):
+        cur = self.con.cursor()
+        # Получили результат запроса, который ввели в текстовое поле
+        item_id = self.spinBox.text()
+        result = self.text.execute("SELECT * FROM Sorts WHERE id=?",
+                                   (item_id,)).fetchall()
+
+
+        # Заполнили размеры таблицы
+        self.tableWidget.setRowCount(len(result))
+        # Если запись не нашлась, то не будем ничего делать
+        if not result:
+            self.statusBar().showMessage('Ничего не нашлось')
+            return
+        else:
+            self.statusBar().showMessage(f"Нашлась запись с id = {item_id}")
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        # Заполнили таблицу полученными элементами
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
+
+    def item_changed(self, item):
+        # Если значение в ячейке было изменено,
+        # то в словарь записывается пара: название поля, новое значение
+        self.modified[self.titles[item.column()]] = item.text()
+
+    def save_results(self):
+        if self.modified:
+            cur = self.con.cursor()
+            que = "UPDATE Sorts SET\n"
+            que += ", ".join([f"{key}='{self.modified.get(key)}'"
+                              for key in self.modified.keys()])
+            que += "WHERE id = ?"
+            print(que)
+            cur.execute(que, (self.spinBox.text(),))
+            self.con.commit()
+            self.modified.clear()
+
+    def add_elem(self):
+        pass
 
 
 if __name__ == '__main__':
